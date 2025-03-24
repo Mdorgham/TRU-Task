3d008c7ed4d58e5977fc225c8b676c705b2a4c1f
Mohamed Dorgham <mohamed.dorghamfciscu@gmail.com>
2025-03-24 23:06:14 +0200

    Initial Commit

---
+++ TRU Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TRU Task
+//
+//  Created by mohamed dorgham on 24/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
